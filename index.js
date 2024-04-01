8519b3f337ca75db0f56995c2650b8fe2ed8555c
GeorgeKahugu <gee.kahugu@gmail.com>
2024-04-01 20:53:28 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+main.remove();
+  
+   
+    var newHeader = document.createElement('h1');
+  
+    
+    newHeader.id = 'victory';
+  
+    
+    var yourName = 'George'; 
+    newHeader.textContent = yourName + ' is the champion';
+  
+   
+    document.body.appendChild(newHeader);
+  
+  
